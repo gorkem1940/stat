c479ddd25136d6d1f85b5f4ded85b8161698031f
gorkem <113816295+gorkem1940@users.noreply.github.com>
2023-05-10 11:15:43 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,7 @@ module.exports = class {
     setInterval(async () => {
       this.client.appInfo = await this.client.fetchApplication();
     }, 60000);
-    this.client.user.setActivity(`Tanrısız ❤️.`);
+    this.client.user.setActivity(`Görkem ❤️.`);
     let botVoiceChannel = this.client.channels.cache.get("862706329923747870");
     if (botVoiceChannel) botVoiceChannel.join().catch(err => console.error("Bot ses kanalına bağlanamadı!"));
     this.client.logger.log(`${this.client.user.tag}, kullanıma hazır ${this.client.users.cache.size} kullanıcı, ${this.client.guilds.cache.size} sunucu.`, "ready");
